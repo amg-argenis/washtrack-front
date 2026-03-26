fb7a06f074e506375340c2315aff931131b0185d
Argenis <argenismuga@gmail.com>
2026-03-26 12:03:58 -0600

    Se agregan rutas para Clientes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,19 @@ import { Routes } from '@angular/router';
 import { ListarordenesComponent } from './componentes/orden/listarordenes/listarordenes.component';
 import { CrearordenComponent } from './componentes/orden/crearorden/crearorden.component';
 import { ActualizarordenComponent } from './componentes/orden/actualizarorden/actualizarorden.component';
+import { ListarClientesComponent } from './features/clientes/listar-clientes/listar-clientes.component';
+import { CrearClienteComponent } from './features/clientes/crear-cliente/crear-cliente.component';
+import { EditarClienteComponent } from './features/clientes/editar-cliente/editar-cliente.component';
 
 export const routes: Routes = [
+    // Ordenes
     { path: 'ordenes/listar', component: ListarordenesComponent },
     { path: 'ordenes/crear', component: CrearordenComponent },
     { path: 'ordenes/editar', component: ActualizarordenComponent },
+    // Clientes
+    { path: 'clientes/listar', component: ListarClientesComponent },
+    { path: 'clientes/crear', component: CrearClienteComponent },
+    { path: 'clientes/editar', component: EditarClienteComponent },
+    // Default
     { path: '', redirectTo: 'ordenes/listar', pathMatch: 'full' }
 ];
