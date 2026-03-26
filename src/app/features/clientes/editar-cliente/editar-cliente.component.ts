52332e6ad049dbc4940ba095414cbc3dd8b9e716
Argenis <argenismuga@gmail.com>
2026-03-26 11:50:37 -0600

    Se agregan componentes para Clientes

---
+++ src/app/features/clientes/editar-cliente/editar-cliente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-cliente',
+  imports: [],
+  templateUrl: './editar-cliente.component.html',
+  styleUrl: './editar-cliente.component.css',
+})
+export class EditarClienteComponent {}
---
+++ src/app/features/clientes/listar-clientes/listar-clientes.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-clientes',
+  imports: [],
+  templateUrl: './listar-clientes.component.html',
+  styleUrl: './listar-clientes.component.css',
+})
+export class ListarClientesComponent {}
