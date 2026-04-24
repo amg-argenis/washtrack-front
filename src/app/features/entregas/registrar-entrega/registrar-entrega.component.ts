39fed388bf9b7e4fbea7357fdc497251977ce799
Argenis <argenismuga@gmail.com>
2026-04-24 13:47:51 -0600

    Entregas - registrar-entregas

---
+++ src/app/features/entregas/registrar-entrega/registrar-entrega.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-entrega',
+  imports: [],
+  templateUrl: './registrar-entrega.component.html',
+  styleUrl: './registrar-entrega.component.css',
+})
+export class RegistrarEntregaComponent {}
