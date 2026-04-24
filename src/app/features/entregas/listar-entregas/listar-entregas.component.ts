7293d6ecfa4aa9f81effe54575989bdd13cffb66
Argenis <argenismuga@gmail.com>
2026-04-24 13:47:14 -0600

    Entrgas - listar-entregas

---
+++ src/app/features/entregas/listar-entregas/listar-entregas.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-entregas',
+  imports: [],
+  templateUrl: './listar-entregas.component.html',
+  styleUrl: './listar-entregas.component.css',
+})
+export class ListarEntregasComponent {}
