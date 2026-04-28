ccb3b0179d459886d5557bca3616c5e694ae79f7
Argenis <argenismuga@gmail.com>
2026-04-27 19:19:31 -0600

    Dashnoard - Inicio

--- src/app/features/dashboard/inicio/inicio.component.ts
+++ src/app/features/dashboard/inicio/inicio.component.ts
@@ -1,9 +1,40 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { CommonModule, DatePipe } from '@angular/common';
+import { DashboardService } from '../../../servidor/dashboard.service';
+import { DashboardResponse } from '../../../models/dashboard/dashboard-response';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [CommonModule, DatePipe],
   templateUrl: './inicio.component.html',
-  styleUrl: './inicio.component.css',
+  styleUrl: './inicio.component.css'
 })
-export class InicioComponent {}
+export class InicioComponent implements OnInit {
+
+  dashboard: DashboardResponse['data'] | null = null;
+  cargando = true;
+  hoy: Date = new Date();
+
+  constructor(
+    private dashboardService: DashboardService,
+    private cdr: ChangeDetectorRef) { }
+
+  ngOnInit(): void {
+    this.cargarDashboard();
+  }
+
+  cargarDashboard() {
+    this.dashboardService.obtenerDashboard().subscribe({
+      next: (response) => {
+        this.cargando = false;
+        if (!response) return;
+        this.dashboard = response.data;
+        this.cdr.detectChanges();
+      },
+      error: (err) => {
+        this.cargando = false;
+        console.error('Error al cargar dashboard:', err);
+      }
+    });
+  }
+}
