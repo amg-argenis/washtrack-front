48451613dd2adbdab7c6ce3f82e4d33b42afd592
Argenis <argenismuga@gmail.com>
2026-04-27 19:00:54 -0600

    Dashboard - Se agrega codigo al service

--- src/app/servidor/dashboard.service.ts
+++ src/app/servidor/dashboard.service.ts
@@ -1,6 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { DashboardResponse } from '../models/dashboard/dashboard-response';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class DashboardService {}
+export class DashboardService {
+
+  private apiUrl = '/washtrack/api/v1';
+
+  constructor(private http: HttpClient) { }
+
+  obtenerDashboard(): Observable<DashboardResponse | null> {
+    return this.http.get<DashboardResponse>(
+      `${this.apiUrl}/busquedas/dashboard`,
+      { observe: 'response' }
+    ).pipe(
+      map(response => {
+        if (response.status === 204 || !response.body) return null;
+        return response.body;
+      })
+    );
+  }
+}
