0148a96720ad60d63a40dad74eaf3525f2675456
Argenis <argenismuga@gmail.com>
2026-05-09 17:24:51 -0600

    Proceso - Component crear proceso

---
+++ src/app/features/procesos/crear-proceso/crear-proceso.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-proceso',
+  imports: [],
+  templateUrl: './crear-proceso.component.html',
+  styleUrl: './crear-proceso.component.css',
+})
+export class CrearProcesoComponent {}
