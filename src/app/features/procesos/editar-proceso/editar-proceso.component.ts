a6e8602fbb7c7122d1045eb7d5dca35c6603c11f
Argenis <argenismuga@gmail.com>
2026-05-09 17:25:14 -0600

    Proceso - Component editar proceso

---
+++ src/app/features/procesos/editar-proceso/editar-proceso.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-proceso',
+  imports: [],
+  templateUrl: './editar-proceso.component.html',
+  styleUrl: './editar-proceso.component.css',
+})
+export class EditarProcesoComponent {}
