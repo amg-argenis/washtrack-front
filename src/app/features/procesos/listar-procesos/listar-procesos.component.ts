bc2f0ff0c4b99ee2e1b1102a3874083cd1adb50d
Argenis <argenismuga@gmail.com>
2026-05-09 17:22:38 -0600

    Se agrega componente procesos

---
+++ src/app/features/procesos/listar-procesos/listar-procesos.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listar-procesos',
+  imports: [],
+  templateUrl: './listar-procesos.component.html',
+  styleUrl: './listar-procesos.component.css',
+})
+export class ListarProcesosComponent {}
