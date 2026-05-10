81519a6ffd75c96e729575fb59c2be782f89b7c7
Argenis <argenismuga@gmail.com>
2026-05-09 19:14:27 -0600

    Proceso - Crear nuevo proceso, Ok

--- src/app/features/procesos/crear-proceso/crear-proceso.component.ts
+++ src/app/features/procesos/crear-proceso/crear-proceso.component.ts
@@ -1,9 +1,56 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ProcesoService } from '../../../servidor/proceso.service';
+import { InsertarProcesoRequest } from '../../../models/procesos/insertar-proceso-request';
 
 @Component({
   selector: 'app-crear-proceso',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './crear-proceso.component.html',
-  styleUrl: './crear-proceso.component.css',
+  styleUrl: './crear-proceso.component.css'
 })
-export class CrearProcesoComponent {}
+export class CrearProcesoComponent {
+
+  guardando = false;
+  errorMsg = '';
+
+  procesoRequest: InsertarProcesoRequest = {
+    nombre: '',
+    descripcion: '',
+    preciounitario: 0
+  };
+
+  constructor(
+    private procesoService: ProcesoService,
+    private router: Router,
+    private cdr: ChangeDetectorRef) { }
+
+  guardar() {
+    this.guardando = true;
+    this.errorMsg = '';
+
+    this.procesoService.insertarProceso(this.procesoRequest).subscribe({
+      next: (response) => {
+        this.guardando = false;
+        if (!response) {
+          this.errorMsg = 'No se recibio respuesta del servidor.';
+          this.cdr.detectChanges();
+          return;
+        }
+        this.router.navigate(['/procesos/listar']);
+      },
+      error: (err) => {
+        this.guardando = false;
+        this.errorMsg = 'Error al crear el proceso. Verifica los datos.';
+        console.error('Error al crear proceso:', err);
+        this.cdr.detectChanges();
+      }
+    });
+  }
+
+  cancelar() {
+    this.router.navigate(['/procesos/listar']);
+  }
+}
