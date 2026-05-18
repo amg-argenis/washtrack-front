fb0c1065a1db997863df3424c0b0ff4c8f367d98
Argenis <argenismuga@gmail.com>
2026-05-18 13:20:12 -0600

    TENANT - registro tenant component

---
+++ src/app/features/auth/registro-tenant/registro-tenant.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-tenant',
+  imports: [],
+  templateUrl: './registro-tenant.component.html',
+  styleUrl: './registro-tenant.component.css',
+})
+export class RegistroTenantComponent {}
