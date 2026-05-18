6110cf0e6b85707f2cd8956257557bb5d9a0708c
Argenis <argenismuga@gmail.com>
2026-05-18 13:20:38 -0600

    TENANT - registro tenant admin component

---
+++ src/app/features/auth/registro-admin/registro-admin.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-admin',
+  imports: [],
+  templateUrl: './registro-admin.component.html',
+  styleUrl: './registro-admin.component.css',
+})
+export class RegistroAdminComponent {}
